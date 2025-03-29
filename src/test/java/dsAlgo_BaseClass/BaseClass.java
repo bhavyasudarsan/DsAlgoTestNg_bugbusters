8df25de0d202aa54396d789e913ff51c6f9172a6
bhavyasudarsan <119521746+bhavyasudarsan@users.noreply.github.com>
2025-03-28 22:19:31 -0400

    added linkedlist cases

--- src/test/java/dsAlgo_BaseClass/BaseClass.java
+++ src/test/java/dsAlgo_BaseClass/BaseClass.java
@@ -1,5 +1,39 @@
 package dsAlgo_BaseClass;
 
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import dsAlgo_DriverFactory.Driver_Factory;
+import dsAlgo_Utilities.ConfigReader;
+
 public class BaseClass {
+	private static final Logger logger = LoggerFactory.getLogger(BaseClass.class);
+
+	Driver_Factory driverFactory;
+	WebDriver driver;
+	
+	public BaseClass() {
+		this.driverFactory = new Driver_Factory();
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		String browser = Driver_Factory.getBrowserType();
+		driver= driverFactory.driverSetup(browser);	
+		driver.get(Driver_Factory.configReader.getUrl());
+		logger.info("Launched the application");
+	}
+	
+	
+
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
 }
