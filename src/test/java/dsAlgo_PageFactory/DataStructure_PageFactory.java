8c6e4f5d6e6a772044434984cfa93d96077c7b0b
sheetalpangare <pangaresheetal@gmail.com>
2025-03-31 00:56:01 -0400

    Added Data Structure module!

--- src/test/java/dsAlgo_PageFactory/DataStructure_PageFactory.java
+++ src/test/java/dsAlgo_PageFactory/DataStructure_PageFactory.java
@@ -1,5 +1,152 @@
 package dsAlgo_PageFactory;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import dsAlgo_DriverFactory.Driver_Factory;
+import dsAlgo_Utilities.ConfigReader;
+import dsAlgo_Utilities.ExcelReader;
+
 public class DataStructure_PageFactory {
+	
+	WebDriver driver ;
+	WebDriverWait wait;
+	String expectedResult;
+	String inputText;
+	ConfigReader configReader = new ConfigReader();
+	
+	public DataStructure_PageFactory() {
+		
+	    driver=  Driver_Factory.getDriverInstance();
+		PageFactory.initElements(driver, this);
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
+	
+	@FindBy(xpath = "//button[@class='btn']")WebElement getStartBtn;
+	@FindBy(linkText = "Get Started")WebElement getStart;
+	@FindBy(linkText = "Sign in")WebElement signIn;
+	@FindBy(id = "id_username")WebElement userName;
+	@FindBy(id = "id_password")WebElement password;
+	@FindBy(xpath = "//input[@type=\"submit\"]")WebElement login;
+	@FindBy(xpath = "//*[@class ='alert alert-primary']")WebElement loginStatus;
+	@FindBy(xpath = "//a[@href='data-structures-introduction']")WebElement data_structureGetSatrted;
+	@FindBy(xpath = "//a[@href='/data-structures-introduction/practice']")WebElement practiceQuestion;
+	@FindBy(xpath = "//a[@href='time-complexity']")WebElement timeComplexity;
+	@FindBy(linkText="Try here>>>")WebElement timeComplexityTryhere;
+	@FindBy(xpath = "//div[@class='CodeMirror-code']")WebElement textEditor;
+	@FindBy(xpath = "//pre[@id='output']")WebElement console;
+	@FindBy(xpath = "//button[text()='Run']")WebElement runButton;
+
+	
+	public void ClickGetStBtn() {
+		getStartBtn.click();
+	}
+
+	public void SignBtn() {
+		signIn.click();
+	}
+	public void enterCredentials(String loginName, String loginPassword) {
+
+		userName.sendKeys(loginName);
+		password.sendKeys(loginPassword);
+	}
+	public void Loginbtn() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOf(login)).click();
+	}
+	
+	public void Login(String Login) throws IOException {
+		
+		List<Object[]> loginData = ExcelReader.readExcelData(Login);
+		Object[] objArray = loginData.get(1);
+		String username = (String) objArray[0];
+		String password = (String) objArray[1];
+		enterCredentials(username,password);
+		Loginbtn();
+	
+	}
+	
+	public String getStatus() {
+
+		return loginStatus.getText();
+	}
+	
+	public void data_structureGetSatrted() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOf(data_structureGetSatrted)).click();
+	}
+	
+	public void practiceQuestion() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(practiceQuestion)).click();
+	}
+	
+	public void timeComplexity() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,500)", "");
+		timeComplexity.click();
+	}
+	
+	public void timeComplexityTryhere() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,600)", "");
+		timeComplexityTryhere.click();
+	}  
+	
+	public String alertMessage() {
+	
+		try {
+			
+		    String alertMessage = driver.switchTo().alert().getText();
+		    driver.switchTo().alert().accept();
+		    System.out.println(alertMessage);
+		    return alertMessage;
+		    
+		} catch (NoAlertPresentException e) {
+			
+			 return "No alert found.";
+		}
+	}
+	
+	public void inputEditor(String code) {
+
+		Actions actions = new Actions(driver);
+		actions.moveToElement(textEditor).click().sendKeys(code).build().perform();
+	}
+	
+	public void runBtnClick() {
+		
+		runButton.click();
+	}
+	
+	public String console() {
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		String output = console.getText();
+		System.out.println(output);
+		return output;
+	}
+	
+	public String getTitle() {
+		
+		return driver.getTitle();	
+	}
 
 }
+
--- src/test/java/dsAlgo_TestClasses/DataStructure_TestClass.java
+++ src/test/java/dsAlgo_TestClasses/DataStructure_TestClass.java
@@ -1,5 +1,102 @@
 package dsAlgo_TestClasses;
 
-public class DataStructure_TestClass {
+import org.testng.annotations.Test;
+import dsAlgo_BaseClass.BaseClass;
+import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import dsAlgo_PageFactory.DataStructure_PageFactory;
+import dsAlgo_TestClasses.DataStructure_TestClass;
+
+public class DataStructure_TestClass extends BaseClass {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DataStructure_TestClass.class);
+	DataStructure_PageFactory dataStructure_PF;
+	
+	@DataProvider (name = "tryEditor_dataProvider")
+	public Object[][] tryEditor_dataProvider(){
+		
+		return new Object[][] {{1},{2},{3}};
+	}
+	
+	@BeforeMethod
+	public void test() throws IOException {
+		
+		dataStructure_PF = new DataStructure_PageFactory();
+		dataStructure_PF.ClickGetStBtn();
+		dataStructure_PF.SignBtn();
+	//	dataStructure_PF.Login("Login");
+		dataStructure_PF.enterCredentials("bugbusters","Team@bug");
+		dataStructure_PF.Loginbtn();
+		dataStructure_PF.data_structureGetSatrted();
+	}
+	
+	@Test(priority = 1, description = "Verify that user should land on the Time Complexity Page")
+	public void timeCompexityPage() {
+		
+		dataStructure_PF.timeComplexity();
+		assertEquals("Time Complexity", dataStructure_PF.getTitle());
+		logger.info("User is in the Time Complexity page");
+	}
+	
+	@Test(priority = 2, description = "Verify that user should land on the Practice Questions Page")
+	public void practiceQuestionsPage() {
+		
+		dataStructure_PF.timeComplexity();
+		dataStructure_PF.practiceQuestion();
+		assertEquals("Practice Questions", dataStructure_PF.getTitle());
+		logger.info("User is in the Practice Questions page");
+	}
+	
+	@Test(priority = 3, description = "Verify that user should land on the Try Editor Page")
+	public void tryEditorPage() {
+		
+		dataStructure_PF.timeComplexity();
+		dataStructure_PF.timeComplexityTryhere();
+		assertEquals("Assessment", dataStructure_PF.getTitle());
+		logger.info("User is in the Practice Questions page");
+	}
+	
+	@Test(priority = 4, dataProvider = "tryEditor_dataProvider")
+	public void tryEditorRunButtonClick(int number) {
+		
+		String inputText;
+		dataStructure_PF.timeComplexity();
+		dataStructure_PF.timeComplexityTryhere();
+		
+		switch (number) {
+		
+		case 1:
+		
+			inputText = "Hiiii";
+			dataStructure_PF.inputEditor(inputText);
+			dataStructure_PF.runBtnClick();
+			String alertMsg= dataStructure_PF.alertMessage();
+			assertEquals("NameError: name 'Hiiii' is not defined on line 1", alertMsg);
+			
+			break;
+
+		case 2:
+			
+			inputText = " ";
+			dataStructure_PF.inputEditor(inputText);
+			dataStructure_PF.runBtnClick();
+			logger.info("No Output displayed");
+			break;
+			
+		case 3:
+			
+			inputText = "print('Hello')";
+			dataStructure_PF.inputEditor(inputText);
+			dataStructure_PF.runBtnClick();
+			String output = dataStructure_PF.console();
+			assertEquals("Hello", output);
+			
+		}
+	}
 }
