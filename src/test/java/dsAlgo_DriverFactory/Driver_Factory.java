5d9b336518b3e2ca9ffe8890f98b7dab15293acb
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-03-31 20:04:20 -0400

    Added Login Test class, Excel and Retry

--- src/test/java/dsAlgo_DriverFactory/Driver_Factory.java
+++ src/test/java/dsAlgo_DriverFactory/Driver_Factory.java
@@ -1,5 +1,61 @@
 package dsAlgo_DriverFactory;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dsAlgo_Utilities.ConfigReader;
+
 public class Driver_Factory {
+
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+	public static ConfigReader configReader = new ConfigReader();
+	private static final Logger logger = LoggerFactory.getLogger(Driver_Factory.class);
+	public final static int TIMEOUT = 2;
+	static String browserType = null;
+
+	public static void setBrowserType(String browser) {
+		browserType = browser;
+	}
+
+	public static String getBrowserType() {
+		if (browserType == null) {
+			try {
+				browserType = configReader.getBrowser();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return browserType;
+	}
+
+	public WebDriver driverSetup(String browser) {
+		if (browser.equalsIgnoreCase("Chrome")) {
+			threadDriver.set(new ChromeDriver());
+		} else if (browser.equalsIgnoreCase("Firefox")) {
+			threadDriver.set(new FirefoxDriver());
+		} else if (browser.equalsIgnoreCase("Edge")) {
+			threadDriver.set(new EdgeDriver());
+		} else {
+			threadDriver.set(new ChromeDriver());
+		}
+		threadDriver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+		threadDriver.get().manage().window().maximize();
+		logger.info("Driver initialised with browser as:  "  +browser) ;
+		return threadDriver.get();
+	}
+
+	public static WebDriver getDriverInstance() {
+		return threadDriver.get();
+	}
+
+	public String getTitle() {
+		return threadDriver.get().getTitle();
+	}
 
 }
--- src/test/java/dsAlgo_TestClasses/Login_TestClass.java
+++ src/test/java/dsAlgo_TestClasses/Login_TestClass.java
@@ -1,5 +1,75 @@
 package dsAlgo_TestClasses;
 
-public class Login_TestClass {
+import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import dsAlgo_Utilities.*;
+import dsAlgo_BaseClass.BaseClass;
+import dsAlgo_PageFactory.Login_PageFactory;
+
+public class Login_TestClass extends BaseClass {
+
+	public Login_PageFactory login_PF;	
+	private static final Logger logger = LoggerFactory.getLogger(Login_TestClass.class);
+		    
+	@BeforeMethod
+	public void test() {
+		logger.info("User is in the Login page");
+		login_PF = new Login_PageFactory();		
+        login_PF.openLogin();
+	}
+	
+	@Test(priority = 1, description = "Verify that user should land on the Login Page")
+	public void Loginpage() {
+		assertEquals("Login", login_PF.getTitle());
+		logger.info("User is in the Sign in page");
+	}
+	
+	@Test(priority = 2, dataProvider = "loginData", retryAnalyzer = RetryAnalyzer.class)
+   public void testLogin(String username, String password, String expectedResult) throws InterruptedException {
+		login_PF.SetUserName(username);
+		login_PF.SetPassword(password);
+		login_PF.ClickBtnLogin();
+
+		if (expectedResult.equals("Please fill out this field."))
+		 {
+			 if (username.equals(""))
+			 {
+				 Assert.assertEquals(expectedResult, login_PF.getValidationMessage(login_PF.inputUsername));
+			 }
+			 else
+			 {
+				 Assert.assertEquals(expectedResult, login_PF.getValidationMessage(login_PF.inputPassword));
+			 }			 
+		 }	 
+		 else
+		 {
+			 Assert.assertEquals(expectedResult, login_PF.getErrMsg()); 
+		 }
+	}
+	
+	@Test(priority = 3, description = "Verify that user able to sign out successfully")
+	public void Logoutpage() {
+		login_PF.SetUserName("bugbusters");
+		login_PF.SetPassword("Team@bug");
+		login_PF.ClickBtnLogin();
+		
+		login_PF.ClickSignOut();
+		Assert.assertEquals("Logged out successfully", login_PF.getErrMsg());
+		logger.info("User Logged out successfully");
+	}
+
+	@DataProvider(name = "loginData")
+    public Object[][] loginDataProvider() throws IOException {
+        return ExcelReader.readExcelData("Login");
+    }
+    
 }
